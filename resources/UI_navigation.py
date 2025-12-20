cb4d465c2f2c220d590032aaaa81a9031d9ca6ec
Walkimist <paulovitortheobald@gmail.com>
2025-12-20 16:59:41 -0300

    Segmented main.py functions and constants into separate files for organization

---
+++ resources/UI_navigation.py
@@ -0,0 +1,58 @@
+import pyautogui
+import time
+
+from screen_coords import COORDS
+from maps_and_gamemodes import *
+
+
+def resetToFirstPage():
+    moveAndClick(COORDS["expert"])
+    moveAndClick(COORDS["beginner"])
+
+
+def changePage(amount):
+    resetToFirstPage()
+    pyautogui.moveTo(COORDS["nextPage"])
+    if amount < 0:
+        amount = abs(amount)
+        pyautogui.moveTo(COORDS["prevPage"])
+
+    for i in range(amount):
+        pyautogui.click()
+
+
+def selectDifficulty(difficulty):
+    moveAndClick(COORDS[difficulty])
+
+
+def selectMode(mode):
+    moveAndClick(COORDS[GAMEMODES[mode]])
+
+
+def selectMap(map, difficulty, mode):
+    changePage(MAPS[map][0])
+    moveAndClick(COORDS[f"slot{MAPS[map][1]}"])
+    selectDifficulty(difficulty)
+    selectMode(mode)
+
+
+def selectLanguage(language):
+    moveAndClick(COORDS["settings"])
+    moveAndClick(COORDS["language"])
+    time.sleep(0.2)
+    moveAndClick(COORDS[language])
+    time.sleep(0.3)
+    moveAndClick(COORDS["back"])
+    moveAndClick(COORDS["back"])
+
+
+def returnToMenu():
+    pyautogui.keyDown("esc")
+    pyautogui.keyUp("esc")
+    time.sleep(0.2)
+    moveAndClick(COORDS["home"])
+
+
+def moveAndClick(pos):
+    pyautogui.moveTo(pos)
+    pyautogui.click()
