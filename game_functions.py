41ab7fd27a209e082b0f33f2bf1ad46f9e74041b
Paulo Vitor Bernardi Theobald <paulovitortheobald@gmail.com>
2025-12-23 03:00:40 -0300

    Refactor game functions for improved readability and structure; streamline money retrieval and tower upgrade logic

--- game_functions.py
+++ game_functions.py
@@ -1,17 +1,23 @@
 from tower_info import TOWER_INFO
 from screen_navigation import moveAndClick
 from screen_coords import MONEY_AREA
+from tower_info import TOWER_INFO
 
 import pyautogui
 import pydirectinput
 import time
 import easyocr
 import warnings
-
-warnings.filterwarnings("ignore", message=".*pin_memory.*")
+import numpy as np
 
 PATH = {"top": ",", "mid": ".", "bot": "/"}
 
+
+warnings.filterwarnings("ignore", message=".*pin_memory.*")
+reader = easyocr.Reader(["en"], gpu=True)
+
+def startRounds():
+    pydirectinput.press('space', 2)
 
 def buyTower(name, coords):
     print(f"PURCHASING {name} AT {coords}")
@@ -19,19 +25,41 @@ def buyTower(name, coords):
     moveAndClick(coords)
 
 
-def upgradeTower(tower, coords, path):
+def upgradeTower(coords, path):
     moveAndClick(coords)
+    time.sleep(0.1)
     pydirectinput.press(PATH[path])
     pyautogui.press("esc")
 
 
 def getCurrentMoney():
-    reader = easyocr.Reader(["en"], gpu=False, verbose=False)
-    pyautogui.screenshot("money_region.png", region=MONEY_AREA)
-    text = reader.readtext("money_region.png", detail=0, allowlist="0123456789,")
-    return int(text[0].replace(",", ""))
+    screenshot = pyautogui.screenshot(region=MONEY_AREA)
+    img = np.array(screenshot)
+    text = reader.readtext(img, detail=0, allowlist="0123456789,")
+    money = 0
+    if text:
+        money = int(text[0].replace(",", ""))
+    if type(money) == int:
+        return money
+    return 0
 
+def changeTargeting(coords, mode):
+    moveAndClick(coords)
+    for i in range(int(mode)):
+        pydirectinput.press('tab')
+    pyautogui.press("esc")
 
-while True:
-    print(getCurrentMoney())
-    time.sleep(1)
+def getTower(towers, command):
+    for tower in towers:
+        if tower.name == command['name'] and tower.id == command['id']:
+            return tower
+
+def getPurchasePrice(command, difficulty, towers):
+    if command['type'] == 'M':
+        return 0
+    if command['type'] == 'B':
+        return TOWER_INFO[command['name']]['price'][difficulty]
+    elif command['type'] == 'U':
+        PATH_FORMAT = {'top': 'top', 'mid': 'middle', 'bot': 'bottom'}
+        upgradePrice = getTower(towers, command).upgrades[command['path']]
+        return TOWER_INFO[command['name']]['upgrades'][PATH_FORMAT[command['path']]][difficulty][upgradePrice]
